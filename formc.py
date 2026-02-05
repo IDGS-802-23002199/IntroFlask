4ef5cf9565871e0a0042e8d1f1840c28c24fe832
EmilioArzo <253627026+IDGS-802-23002199@users.noreply.github.com>
2026-02-05 14:19:29 -0600

    cinepolis

---
+++ formc.py
@@ -0,0 +1,18 @@
+# from wtforms import Form
+# from wtforms import StringField, IntegerField, PasswordField, RadioField
+# from wtforms import EmailField
+# from wtforms import validators
+
+
+# class UserFormc(Form):
+#     matricula=IntegerField('Nombre', [validators.DataRequired(message="El campo es requerido"),
+#                                         validators.DataRequired(message='El campo es requerido')
+#                                         ])
+#     nombre=StringField('Cantidad', [validators.DataRequired(message="El campo es requerido"),
+#                                         validators.NumberRange(min=1, max=7, message='Ingrese un número válido')
+#                                         ])
+#     apaterno=RadioField('Cineco',[validators.DataRequired(message="El campo es requerido")
+#                                         ],
+#                                 choices=[('SI','Si'),('NO','No')],
+#                                         default='NO')
+
